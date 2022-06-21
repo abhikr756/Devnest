ca34a3fb54875e1de77e15181449484c2d2007df
Abhishek <abhikr756@gmail.com>
2022-06-21 23:06:19 +0530

    Problem 3

---
+++ DSA_Bootcamp/3_product_of_division.java
@@ -0,0 +1,11 @@
+package DSA_Bootcamp;
+
+public class 3_product_of_division {
+    static float solve(int a, int b){
+        //CODE HERE 
+        int c=a/b;
+        float d=(float)a/b;
+        float prod=c*d;
+        return prod;
+        }
+}
